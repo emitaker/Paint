cef0ec2763d9272909c5f1b0fed494db7300a3ad
LorenaPalominoCastillo <65177726+LorenaPalominoCastillo@users.noreply.github.com>
2021-05-05 20:33:48 -0500

    Se añadieron comentarios a las ultimas lineas de codigo

--- paint.py
+++ paint.py
@@ -36,16 +36,15 @@ def square(start, end):
 def circle(start, end):
     import turtle
     "Draw circle from start to end."
-<<<<<<< HEAD
+
     up()
     goto(start.x, start.y)
     down()
     begin_fill()
     turtle.circle(end.x - start.y)
     end_fill()
-=======
-    pass
->>>>>>> be90397383725b6a75aaa033abb3eccee09386a0
+
+
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
@@ -96,15 +95,27 @@ setup(420, 420, 370, 0)
 onscreenclick(tap)
 listen()
 onkey(undo, 'u')
+"Se presiona la tecla K para cambiar a color negro"
 onkey(lambda: color('black'), 'K')
+"Se presiona la tecla W para cambiar a color blanco"
 onkey(lambda: color('white'), 'W')
+"Se presiona la tecla G para cambiar a color verde"
 onkey(lambda: color('green'), 'G')
+"Se presiona la tecla B para cambiar a color azul"
 onkey(lambda: color('blue'), 'B')
+"Se presiona la tecla R para cambiar a color rojo"
 onkey(lambda: color('red'), 'R')
+"Se presiona la tecla Y para cambiar a color amarillo"
 onkey(lambda: color('yellow'), 'Y')
+"Se presiona la tecla l para cambiar la figura a una linea"
 onkey(lambda: store('shape', line), 'l')
+"Se presiona la tecla s para cambiar la figura a una cuadrado"
 onkey(lambda: store('shape', square), 's')
+"Se presiona la tecla c para cambiar la figura a una circulo"
 onkey(lambda: store('shape', circle), 'c')
+"Se presiona la tecla r para cambiar la figura a una rectangulo"
 onkey(lambda: store('shape', rectangle), 'r')
+"Se presiona la tecla t para cambiar la figura a una triangulo"
 onkey(lambda: store('shape', triangle), 't')
+
 done()
